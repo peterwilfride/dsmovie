4c067010cab650c152a55f52c5145d93686d0b28
peterwilfride <cybermind545@gmail.com>
2022-05-15 10:47:20 -0300

    pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -30,9 +30,14 @@ function Listing() {
         })
     }, [pageNumber])
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber)
+    }
+
     return (
         <>
-            <Pagination></Pagination>
+            <Pagination page={page} onChange={handlePageChange}></Pagination>
+
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
